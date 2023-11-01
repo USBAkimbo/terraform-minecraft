3f6d3eb099602ccff510a7e3cc1ee739c6c2f2aa
77cla77@gmail.com <77cla77@gmail.com>
2023-11-01 18:23:02 +0000

    moved vm to separate config

--- main.tf
+++ main.tf
@@ -32,36 +32,6 @@ data "oci_identity_availability_domain" "ad" {
   ad_number      = var.availability_domain_number
 }
 
-# Create VM
-resource "oci_core_instance" "vm" {
-  availability_domain                 = var.availability_domain
-  compartment_id                      = var.compartment_ocid
-  display_name                        = var.vm_name
-  shape                               = var.vm_shape
-  is_pv_encryption_in_transit_enabled = "true"
-
-  shape_config {
-    ocpus         = var.vm_ocpus
-    memory_in_gbs = var.vm_ram
-  }
-
-  create_vnic_details {
-    subnet_id        = oci_core_subnet.vcn-subnet.id
-    display_name     = "primaryvnic"
-    assign_public_ip = true
-    hostname_label   = var.vm_name
-  }
-
-  source_details {
-    source_type = "image"
-    source_id   = var.vm_os_image
-  }
-
-  metadata = {
-    "ssh_authorized_keys" = "${var.ssh_key}"
-  }
-}
-
 # Null resource to always run Ansible
 resource "null_resource" "ansible" {
 
