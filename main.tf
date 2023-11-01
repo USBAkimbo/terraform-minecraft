b6a6b5ff774b3d99a3f1ff20ba881df8ee542dca
77cla77@gmail.com <77cla77@gmail.com>
2023-11-01 18:02:59 +0000

    moved vars to variables.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,3 @@
-# Account info vars
-variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
-variable "public_key_path" {}
-variable "compartment_ocid" {}
-variable "region" {}
-variable "availability_domain" {}
-variable "availability_domain_number" {}
-
-# Compute vars
-variable "vm_name" {}
-variable "vm_shape" {}
-variable "vm_ocpus" {}
-variable "vm_ram" {}
-variable "vm_os_image" {}
-variable "ssh_key" {}
-
-# Networking vars
-variable "net_vcn_name" {}
-variable "net_subnet_name" {}
-variable "net_cidr_block" {}
-variable "net_subnet" {}
-
-# Cloudflare vars
-variable "cf_api_token" {}
-variable "cf_zone_id" {}
-variable "cf_dns_minecraft" {}
-
 # Configure provider version
 terraform {
   required_providers {
